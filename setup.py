2ed18f4a734f2621060957dfbceabe640378c476
Aurelien Campeas <aurelien.campeas@pythonian.fr>
2018-06-18 15:53:23 +0200

    pkg: fixed a packaging issue

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='rework_ui',
-      version='0.1.0',
+      version='0.1.1',
       author='Aurelien Campeas',
       author_email='aurelien.campeas@pythonian.fr',
       description='A web ui for the rework distributed task dispatcher',
