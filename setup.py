4faf05d0e8124016375754d780b3a248f9c250ae
Aurelien Campeas <aurelien.campeas@pythonian.fr>
2018-07-12 15:01:15 +0200

    pkg: prepare 0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='rework_ui',
-      version='0.1.1',
+      version='0.2.0',
       author='Aurelien Campeas',
       author_email='aurelien.campeas@pythonian.fr',
       description='A web ui for the rework distributed task dispatcher',
