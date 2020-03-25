2e8af840f41c2e5a4831cbfc635c43b51b9d2685
André Espaze <dede@pythonian.fr>
2020-03-25 15:53:39 +0100

    Decoding taskHello

--- elm/tests/Suite.elm
+++ elm/tests/Suite.elm
@@ -1,39 +1,81 @@
-module Suite exposing (suite)
+module Suite exposing (testParser)
 
-import Either
 import Expect
 import Json.Decode as D
-import Main exposing (Task, taskDecoder)
-import Test exposing (Test)
+import Test as T
 
 
-type alias T =
-    { name : String
-    , json : String
-    , result : List Task
+type alias Task =
+    { id : Int
+    , name : String
+    , domain : String
+    , queued : String
+    , started : String
+    , finished : String
+    , worker : Int
     }
 
 
-tests : List T
-tests =
-    [ T "start" "[]" []
-    ]
+inputHello : String
+inputHello =
+    """
+[
+  {
+    "tid": 1,
+    "name": "hello",
+    "status": "done",
+    "abort": false,
+    "domain": "default",
+    "operation": 1,
+    "queued": "2020-03-24 20:24:37+0100",
+    "started": "2020-03-24 20:24:37+0100",
+    "finished": "2020-03-24 20:24:51+0100",
+    "metadata": null,
+    "worker": 14,
+    "deathinfo": "Got a TERMINATE/15 signal while at <frame at 0x7f9640e74af8, file '/home/pythonian/src/rework/rework/monitor.py', line 523, code _run>",
+    "traceback": null
+  }
+]
+"""
 
 
-suite : Test
-suite =
+taskHello : Task
+taskHello =
+    Task
+        1
+        "hello"
+        "default"
+        "2020-03-24 20:24:37+0100"
+        "2020-03-24 20:24:37+0100"
+        "2020-03-24 20:24:51+0100"
+        14
+
+
+taskDecoder : D.Decoder Task
+taskDecoder =
+    D.map7
+        Task
+        (D.field "tid" D.int)
+        (D.field "name" D.string)
+        (D.field "domain" D.string)
+        (D.field "queued" D.string)
+        (D.field "started" D.string)
+        (D.field "finished" D.string)
+        (D.field "worker" D.int)
+
+
+testParser : T.Test
+testParser =
     let
-        runTest : T -> Test
-        runTest x =
-            let
-                testDecoder : Expect.Expectation
-                testDecoder =
-                    D.decodeString (D.list taskDecoder) x.json
-                        |> Either.fromResult
-                        |> Either.unpack
-                            (D.errorToString >> Expect.fail)
-                            (Expect.equal x.result)
-            in
-            Test.test x.name (\_ -> testDecoder)
+        testDecoder : String -> Result D.Error (List Task)
+        testDecoder jsonString =
+            D.decodeString (D.list taskDecoder) jsonString
     in
-    List.map runTest tests |> Test.concat
+    T.describe "testParser"
+        [ T.test "taskHello"
+            (\_ ->
+                Expect.equal
+                    (testDecoder inputHello)
+                    (Ok [ taskHello ])
+            )
+        ]
