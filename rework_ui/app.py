47c3fc0d2f04b0eea7f3193a90dd5ae5295052e5
Aurelien Campeas <aurelien.campeas@pythonian.fr>
2020-03-27 17:19:23 +0100

    app: accept a prefix param

--- rework_ui/app.py
+++ rework_ui/app.py
@@ -4,9 +4,12 @@ from flask import Flask
 from rework_ui.blueprint import reworkui
 
 
-def make_app(engine):
+def make_app(engine, prefix=None):
     app = Flask('rework')
-    app.register_blueprint(reworkui(engine))
+    app.register_blueprint(
+        reworkui(engine),
+        url_prefix=prefix
+    )
     return app
 
 
