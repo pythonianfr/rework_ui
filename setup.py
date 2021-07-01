970a3516d341e32f1737888457709c1fb1f9b565
Aurelien Campeas <aurelien.campeas@pythonian.fr>
2021-07-01 19:25:49 +0200

    pkg: build the elm files at pip install time

--- setup.py
+++ setup.py
@@ -1,5 +1,30 @@
+import subprocess
 from pathlib import Path
 from setuptools import setup
+from setuptools.command.build_ext import build_ext
+
+
+WORKING_DIR = Path(__file__).resolve().parent
+STATIC_DIR = WORKING_DIR / 'rework_ui' / 'rui_static'
+
+
+def compile_elm(edit_kind, src):
+    src = WORKING_DIR / 'elm/src' / src
+    out = STATIC_DIR / f'{edit_kind}.js'
+    cmd = f'cd elm && elm make --optimize --output {out} {src}'
+    print(cmd, subprocess.call(cmd, shell=True))
+
+
+class ElmBuild(build_ext):
+
+    def run(self):
+        for edit_kind, src in [
+            ('rework_ui_elm', 'Main.elm'),
+            ('logview', 'Logview.elm'),
+            ('info', 'Info.elm'),
+        ]:
+            compile_elm(edit_kind, src)
+        super().run()
 
 
 doc = Path(__file__).parent / 'README.md'
@@ -42,5 +67,6 @@ setup(name='rework_ui',
           'Environment :: Web Environment',
           'Topic :: System :: Distributed Computing',
           'Topic :: Software Development :: User Interfaces'
-      ]
+      ],
+      cmdclass={'build_ext': ElmBuild}
 )
